eb43375b891bbbdaf79e39798ba49803eb23cbdf
Tomas Garcia <tomasotano25@gmail.com>
2018-05-15 00:48:56 -0400

    deploy

--- webpack.config.js
+++ webpack.config.js
@@ -4,13 +4,47 @@ const config = {
   resolve: {
     modules: [path.resolve('./lib'), path.resolve('./node_modules')],
   },
-  entry: ['babel-polyfill', './lib/renderers/dom.js'],
+  // entry: ['babel-polyfill', './lib/renderers/dom.js'],
+  entry: {
+    vendor: [
+      'babel-polyfill',
+      'react',
+      'react-dom',
+      'prop-types',
+      'styled-components',
+      'axios',
+      'lodash.debounce',
+      'lodash.pickby',
+    ],
+    app: ['./lib/renderers/dom.js'],
+  },
   output: {
     path: path.resolve(__dirname, 'public'),
-    filename: 'bundle.js',
+    filename: '[name].js',
   },
   module: {
-    rules: [{ test: /\.(js|jsx)$/, exclude: /node_modules/, use: 'babel-loader' }],
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: { loader: 'babel-loader' },
+      },
+    ],
+  },
+  optimization: {
+    minimize: true,
+    runtimeChunk: {
+      name: 'vendor',
+    },
+    splitChunks: {
+      cacheGroups: {
+        default: false,
+        commons: {
+          name: 'vendor',
+          chunks: 'initial',
+        },
+      },
+    },
   },
 };
 
